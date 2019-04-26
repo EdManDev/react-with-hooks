c977a028309a120be02ce56c6a59497f5a64e75a
EdManDev <ed.manigat@gmail.com>
2019-04-26 13:09:30 -0400

    gohahead

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,93 @@
 import React, { useState } from "react";
+import "./App.css";
 
-function Main() {
-  // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
+// ****** I can do this in diferent  way  ***************
+//  const Main = () => {
+// }
+// ***********  OR **************************************
+// function Main() {
+// }
 
+function Todo({ todo, index, completeTodo }) {
   return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+    <div
+      style={{ textDecoration: todo.isCompleted ? "line-throught" : "" }}
+      className="todo"
+    >
+      {todo.text}
+      <span />
+      <div>
+        <button onClick={() => completeTodo(index)}>Complet</button>
+      </div>
     </div>
   );
 }
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+        placeholder="Add Todo..."
+      />
+    </form>
+  );
+}
+
+const Main = () => {
+  const [todos, setTodos] = useState([
+    {
+      text: "Learn about React",
+      isComplet: false
+    },
+    {
+      text: "Meet friend for lunch",
+      isComplet: false
+    },
+    {
+      text: "build really cool too app",
+      isComplet: false
+    }
+  ]);
+
+  const addTodo = text => {
+    const NewTodos = [...todos, { text }];
+    setTodos(NewTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+};
+
+export default Main;
